0466a0c7a9b0d90ad3476ccd0440bc6314f56014
전홍현 <toinbee3@naver.com>
2024-08-12 13:42:32 +0900

    [Feat] 방 들어가기 기능 구현

--- Assets/3.Script/Network/tempNetworkUI.cs
+++ Assets/3.Script/Network/tempNetworkUI.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 using Mirror;
 
 public class tempNetworkUI : MonoBehaviour {
+    // 임시로 네트워크 기능 테스트하기 위해서
+    // 버튼 할당 메서드를 작성한 스크립트
 
     public void OnHostButtonClicked() {
+        // 게임 방 만들기 버튼에 할당되는 메서드
         CreateHostRoom();
     }
 
     private void CreateHostRoom() {
         var roomManager = RoomManager.singleton;
-
+        //TODO: 방 설정 메서드 처리 필요
 
         roomManager.StartHost();
+    }
+
+    public void OnJoinButtonClicked() {
+        JoinRoom();
+    }
+
+    private void JoinRoom() {
+        var roomManager = RoomManager.singleton;
+        roomManager.StartClient();
     }
 
 }
