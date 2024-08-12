54ef863868bd5172f3c1f6176e169c838f71cb29
전홍현 <toinbee3@naver.com>
2024-08-12 14:34:27 +0900

    [Feat] Player Move Network 동기화 구현

---
+++ Assets/3.Script/Player/PlayerMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Mirror;
+
+public class PlayerMove : NetworkBehaviour {
+    private float MoveSpeed = 5f;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void Move() {
+        if (!isOwned) return;
+        float inputX = Input.GetAxisRaw("Horizontal");
+        Vector3 moveDirection = Vector3.ClampMagnitude(new Vector3(inputX, 0, 0), 1f);
+
+        if (moveDirection.x < 0f) transform.localScale = new Vector3(-1f, 1f, 1f);
+        else if (moveDirection.x > 0f) transform.localScale = new Vector3(1f, 1f, 1f);
+
+        transform.position += moveDirection * MoveSpeed * Time.deltaTime;
+    }
+}
