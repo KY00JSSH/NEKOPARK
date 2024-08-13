38e6309d57015588a83490062ad1023fa1712b1c
김수주 <tnwn1122@naver.com>
2024-08-13 21:35:11 +0900

    [fix] playermove 일부 수정

--- Assets/3.Script/Player/PlayerMove.cs
+++ Assets/3.Script/Player/PlayerMove.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 using Mirror;
 using UnityEngine.UI;
 
-public class PlayerMove : NetworkBehaviour 
-{
+public class PlayerMove : NetworkBehaviour {
     private float moveSpeed = 5f;
-    public bool IsMovingRight { get; private set; }
+    public bool IsMoving { get; private set; }
 
     private float jumpForce = 400f;
-   
+
     private Rigidbody2D playerRigidbody;
     private Collider2D playerCollider;
 
@@ -16,17 +15,15 @@ public class PlayerMove : NetworkBehaviour
 
     private Text textNickname;
 
-    private void Awake()
-    {
+    private void Awake() {
         playerRigidbody = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         playerCollider = GetComponent<Collider2D>();
         textNickname = GetComponentInChildren<Text>();
     }
 
-    private void Update()
-    {
-        Jump();        
+    private void Update() {
+        Jump();
     }
 
     private void FixedUpdate() {
@@ -37,22 +34,20 @@ public class PlayerMove : NetworkBehaviour
     private void Move() {
         //if (!isOwned) return;
 
-        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            IsMovingRight = true;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            IsMoving = true;
             transform.localScale = new Vector3(-1f, 1f, 1f);
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
             playerAnimator.SetBool("isMoving", true);
         }
-        else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            IsMovingRight = false;
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            IsMoving = true;
             transform.localScale = new Vector3(1f, 1f, 1f);
             transform.position += Vector3.left * moveSpeed * Time.deltaTime;
             playerAnimator.SetBool("isMoving", true);
         }
-        else
-        {
+        else {
+            IsMoving = false;
             playerAnimator.SetBool("isMoving", false);
         }
 
@@ -65,32 +60,27 @@ public class PlayerMove : NetworkBehaviour
             textNickname.transform.localScale = textScale;
     }
 
-    private void Jump()
-    {
+    private void Jump() {
         //if (!isOwned) return;
 
-        if (Input.GetKey(KeyCode.Space) && !playerAnimator.GetBool("isJumping"))
-        {            
-            playerRigidbody.AddForce(new Vector2(0, jumpForce));            
+        if (Input.GetKey(KeyCode.Space) && !playerAnimator.GetBool("isJumping")) {
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
             playerAnimator.SetBool("isJumping", true);
             AudioManager.instance.PlaySFX(AudioManager.Sfx.jump);
-        }       
+        }
     }
 
     private void Jump_Limit() {
         //if (!isOwned) return;
 
-        if (playerRigidbody.velocity.y < 0)
-        {
-            Vector2 feetPosition = 
+        if (playerRigidbody.velocity.y < 0) {
+            Vector2 feetPosition =
                 new Vector2(playerRigidbody.position.x, playerRigidbody.position.y - playerCollider.bounds.extents.y - 0.3f);
 
             Debug.DrawRay(feetPosition, Vector3.down * 0.1f, new Color(0, 1, 0));
             RaycastHit2D rayHit = Physics2D.Raycast(feetPosition, Vector2.down, 1f);
-            if (rayHit.collider != null)
-            {
-                if (rayHit.distance <= 0.1f)
-                {
+            if (rayHit.collider != null) {
+                if (rayHit.distance <= 0.1f) {
                     playerAnimator.SetBool("isJumping", false);
                 }
             }
