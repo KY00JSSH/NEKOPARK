20c9497351266389d8e41dc5947e47a491bf0f37
전홍현 <toinbee3@naver.com>
2024-08-13 13:23:27 +0900

    [Feat] Host 방 설정 기능 구현

--- Assets/3.Script/Network/RoomPlayer.cs
+++ Assets/3.Script/Network/RoomPlayer.cs
@@ -11,6 +11,11 @@ public class RoomPlayer : NetworkRoomPlayer {
     public override void Start() {
         base.Start();
         if(isServer) SpawnRoomPlayer();
+        RoomManager.UpdateConnenctedPlayerCount();
+    }
+
+    private void OnDestroy() {
+        RoomManager.UpdateConnenctedPlayerCount();
     }
 
     private void SpawnRoomPlayer() {
@@ -44,26 +49,6 @@ public class RoomPlayer : NetworkRoomPlayer {
                 break;
             }
         }
-
-        /*
-        for (int i = 0; i < Enum.GetValues(typeof(PlayerColorType)).Length; i++) {
-            bool isSameColor = false;
-            foreach(var roomplayer in roomSlots) {
-                var player = roomplayer as RoomPlayer;
-                if (player.playerColor == (PlayerColorType)i &&
-                    roomplayer.netId != netId) {                    // 내가 아닌 다른 플레이어가 
-                    isSameColor = true;
-                    break;
-                }
-            }
-            if (isSameColor) continue;
-            else {
-                playerColor = (PlayerColorType)i;
-                break;
-            }
-        }
-        */
-
         return playerColor;
     }
 
