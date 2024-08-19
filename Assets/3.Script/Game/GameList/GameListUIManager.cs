1ac6d19f086ada60a8e20c3a1d4fec568651e6e0
김수진 <nuu042@naver.com>
2024-08-19 12:57:52 +0900

    [Feat] 게임 2-2부터 dlc 처리

--- Assets/3.Script/Game/GameList/GameListUIManager.cs
+++ Assets/3.Script/Game/GameList/GameListUIManager.cs
@@ -16,35 +16,48 @@ public class GameListUIManager : MonoBehaviour {
     public void OpenMainList() {
         canvas[0].gameObject.SetActive(true);
         canvas[1].gameObject.SetActive(false);
+        canvas[2].gameObject.SetActive(false);
     }
 
     public void OpenDetailList(int index) {
         canvas[0].gameObject.SetActive(false);
         canvas[1].gameObject.SetActive(true);
+        canvas[2].gameObject.SetActive(false);
         GameListManager.instance.SetMajorStageIndex(index);
     }
 
     public void OpenDetailListReturn() {
         canvas[0].gameObject.SetActive(false);
         canvas[1].gameObject.SetActive(true);
+        canvas[2].gameObject.SetActive(false);
     }
 
     public void AllCloseUI() {
         canvas[0].gameObject.SetActive(false);
         canvas[1].gameObject.SetActive(false);
+        canvas[2].gameObject.SetActive(false);
+    }
+
+    public void CloseDLCDialog() {
+        canvas[2].gameObject.SetActive(false);
     }
 
     public void OpenGame(int index) {
         GameListManager.instance.SetMinorStageIndex(index);
-        Debug.Log("WHAT");
-        if (GameListManager.instance.IsLocalGame) {
-            AllCloseUI();
-            AudioManager.instance.StopBGM();
-            SceneManager.LoadScene($"Game_{GameListManager.instance.MajorStageIndex + 1}-{GameListManager.instance.MinorStageIndex + 1}");
+        if ((GameListManager.instance.MajorStageIndex == 1 && index > 0) || GameListManager.instance.MajorStageIndex > 1) {
+            canvas[2].gameObject.SetActive(true);
         }
         else {
-            AllCloseUI();
-            OpenGameScene();
+            if (GameListManager.instance.IsLocalGame) {
+                AllCloseUI();
+                AudioManager.instance.StopBGM();
+            
+                SceneManager.LoadScene($"Game_{GameListManager.instance.MajorStageIndex + 1}-{GameListManager.instance.MinorStageIndex + 1}");
+            }
+            else {
+                AllCloseUI();
+                OpenGameScene();
+            }
         }
     }
 
@@ -54,7 +67,7 @@ public class GameListUIManager : MonoBehaviour {
 
         foreach (RoomPlayer player in roomManager.roomSlots)
             player.ReadyStateChanged(false, true);
-        Debug.Log("WOW");
+
         roomManager.ServerChangeScene($"Game_{GameListManager.instance.MajorStageIndex + 1}-{GameListManager.instance.MinorStageIndex + 1}");
         //roomManager.ServerChangeScene(roomManager.Game_1_1);
     }
